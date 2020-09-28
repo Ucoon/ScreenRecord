4f4da0a29fb696dd43f15b1b50d60eae89c013aa
hezongjie <hezongjie@somo.tech>
2020-09-28 11:07:36 +0800

    feat: 提交截图功能

--- app/src/main/java/tech/ucoon/screenrecord/MainActivity.java
+++ app/src/main/java/tech/ucoon/screenrecord/MainActivity.java
@@ -6,28 +6,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.PixelFormat;
 import android.graphics.Point;
 import android.hardware.display.DisplayManager;
 import android.hardware.display.VirtualDisplay;
+import android.media.Image;
+import android.media.ImageReader;
 import android.media.projection.MediaProjection;
 import android.media.projection.MediaProjectionManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.SystemClock;
 import android.view.Display;
+import android.view.Surface;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.nio.ByteBuffer;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button mStartScreenRecorderBtn;
     private Button mStopScreenRecorderBtn;
+    private ImageView imageView;
     private SurfaceView mSurfaceView;
 
     private MediaProjectionManager projectionManager;
     private MediaProjection mMediaProjection;
+    private ImageReader mImageReader;
     private VirtualDisplay mVirtualDisplay;
+    private Handler mHandler;
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +57,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mStopScreenRecorderBtn = findViewById(R.id.btnStopScreenRecorder);
         mStopScreenRecorderBtn.setOnClickListener(this);
         mSurfaceView = findViewById(R.id.surfaceView);
+        imageView = findViewById(R.id.imgIcon);
     }
 
     private Intent getCaptureIntent(){
@@ -53,11 +68,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mMediaProjection = projectionManager.getMediaProjection(resultCode, data);
     }
 
-    private void createVirtualDisplay(){
+    private void createVirtualDisplay(Surface surface){
         int[] screenSize = getPhysicalResolution(this);
         mVirtualDisplay = mMediaProjection.createVirtualDisplay("Display-" + SystemClock.uptimeMillis(),
                 upTo16X(screenSize[0]/2), upTo16X(screenSize[1]/2), 1,
-                DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC, mSurfaceView.getHolder().getSurface(), null, null);
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC, surface, null, null);
     }
 
     private void releaseMediaProjection(){
@@ -93,6 +108,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void stopScreenRecorder(boolean destroy){
         releaseSurfaceView(destroy);
         releaseVirtualDisplay();
+        releaseImageReader(mImageReader);
+        releaseCaptureHandler();
         releaseMediaProjection();
     }
 
@@ -135,9 +152,98 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (requestCode == 1024) {
             if (resultCode == RESULT_OK && data != null){
                 createMediaProjection(resultCode, data);
-                createVirtualDisplay();
+                createCaptureHandler();
+                Surface surface = createImageReader();
+                createVirtualDisplay(surface);
             }
         }
+    }
+
+    private void createCaptureHandler(){
+        if (mHandler == null){
+            HandlerThread handlerThread = new HandlerThread("screen-capture");
+            handlerThread.start();
+            mHandler = new Handler(handlerThread.getLooper());
+        }
+    }
+
+    private void releaseCaptureHandler(){
+        try {
+            Handler handler = mHandler;
+            if (handler != null){
+                handler.getLooper().quitSafely();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            mHandler = null;
+        }
+    }
+
+    private Surface createImageReader(){
+        int[] screenSize = getPhysicalResolution(this);
+        ImageReader imageReader = ImageReader.newInstance(upTo16X(screenSize[0]/2), upTo16X(screenSize[1]/2),
+                PixelFormat.RGBA_8888, 2);
+        imageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
+            @Override
+            public void onImageAvailable(ImageReader reader) {
+                Image image = null;
+                try {
+                    image = reader.acquireNextImage();
+                    Image.Plane plane = image.getPlanes()[0];
+                    ByteBuffer buffer = plane.getBuffer();
+                    int pixelStride = plane.getPixelStride();
+                    int rowStride = plane.getRowStride();
+                    int width = image.getWidth();
+                    int height = image.getHeight();
+                    int rowPadding = rowStride - pixelStride * width;
+                    bitmap = Bitmap.createBitmap(width + rowPadding / pixelStride, height, Bitmap.Config.ARGB_8888);
+                    bitmap.copyPixelsFromBuffer(buffer);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    if (image != null){
+                        try {
+                            image.close();
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                    if (bitmap != null){
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                imageView.setImageBitmap(bitmap);
+                            }
+                        });
+                    }
+                }
+            }
+        }, mHandler);
+        synchronized (MainActivity.this){
+            mImageReader = imageReader;
+        }
+        return imageReader.getSurface();
+    }
+
+    private void releaseImageReader(final ImageReader imageReader){
+        if (imageReader == null || mHandler == null) return;
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    imageReader.close();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    synchronized (MainActivity.this){
+                        if (imageReader == mImageReader){
+                            mImageReader = null;
+                        }
+                    }
+                }
+            }
+        });
     }
 
     /**
